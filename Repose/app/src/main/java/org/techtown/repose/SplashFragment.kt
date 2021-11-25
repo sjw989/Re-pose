70f3836bd3b81e276f51f9a1a0b1723c49d490bb
kijung <daiywlf5251@gmail.com>
2021-11-25 23:26:41 +0900

    temp implement Login Feature
    just insert roomDB
    todo: node server id & pw match check

--- Repose/app/src/main/java/org/techtown/repose/SplashFragment.kt
+++ Repose/app/src/main/java/org/techtown/repose/SplashFragment.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +13,19 @@ import android.view.animation.Animation
 import androidx.core.animation.doOnEnd
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.techtown.repose.databinding.FragMainBinding
 import org.techtown.repose.databinding.FragSplashBinding
+import kotlin.coroutines.coroutineContext
 
 
 class SplashFragment : Fragment() {
     lateinit var  navController : NavController// 네비게이션 컨트롤러
     private var _binding : FragSplashBinding? = null // 뷰바인딩
     private val binding get() = _binding!! // 뷰바인딩
+    private var ExistUser: Boolean = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -29,11 +35,18 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val mc : MainActivity = MainActivity()
+        mc.db = AppDatabase.getInstance(requireContext())!!
         navController = Navigation.findNavController(view) // 네비게이션 컨트롤러 view로 부터 가져오기
-        init()
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.e("Exist User",ExistUser.toString())
+            ExistUser = mc.db.userDao().getUserData() != null
+            Log.e("Exist User",ExistUser.toString())
+            init()
+            Log.e("Exist User",ExistUser.toString())
 
+        }
     }
-
 
     private fun init(){ // 앱 시작할때 애니메이션 효과
         var fadeIn = ObjectAnimator.ofFloat(binding.logoImageView, "alpha", 0f, 1f)
@@ -51,12 +64,11 @@ class SplashFragment : Fragment() {
             fadeOut.duration = 1500
             fadeOut.start()
             fadeOut.doOnEnd {
-                navController.navigate(R.id.action_frag_splash_to_frag_start)
+                //룸디비 체크
+                if(ExistUser) navController.navigate(R.id.action_frag_splash_to_frag_main)
+                else navController.navigate(R.id.action_frag_splash_to_frag_start)
             }
         }, 4000)
-
-
-
     }
     // 뷰바인딩 destroy
     override fun onDestroyView() {
--- Repose/app/src/main/java/org/techtown/repose/signIn/LoginFragment.kt
+++ Repose/app/src/main/java/org/techtown/repose/signIn/LoginFragment.kt
@@ -1,6 +1,7 @@
 package org.techtown.repose.signIn
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +10,13 @@ import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
 import kotlinx.android.synthetic.main.frag_login.*
-import org.techtown.repose.MainFragment
-import org.techtown.repose.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.techtown.repose.*
 
 
 class LoginFragment : Fragment() {
@@ -26,10 +31,23 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var mc : MainActivity = MainActivity()
+
+        //외부 노드서버랑 연동하기 전 임시 데이터
+        val newUserData = UserData("testuser","pw","email", mutableListOf<Boolean>(false,false,false,false,false,false,false),
+            mutableListOf<Boolean>(false,false,false,false,false,false,false), mutableListOf<Boolean>(false,false,false,false,false,false,false),
+            mutableListOf<Boolean>(false,false,false,false,false,false,false),0,false,"20211125")
+
+        mc.db = AppDatabase.getInstance(requireContext())!!
         back_pressed() // 뒤로가기 버튼
         login_btn.setOnClickListener{
-            //로그인을 했을 때 서버에서 아이디랑 비밀번호 일치여부 -> 가져와서 룸DB에 저장한 뒤 main fragment로 이동
-            findNavController().navigate(R.id.action_frag_login_to_frag_main)
+            //TO DO: 서버랑 아이디 비밀번호 일치하는지 체크
+            //아이디 비밀번호 일치시, 룸디비에 데이터 넣기
+            CoroutineScope(Dispatchers.IO).launch {
+                // room DB 체크해서 넘어갈지 말지
+                RoomDBInsertUserData(mc, newUserData)
+                MoveMainFragment()
+            }
         }
 //        login_btn.setOnClickListener{
 //            //로그인을 했을 때 서버에서 아이디랑 비밀번호 일치여부 -> 가져와서 룸DB에 저장한 뒤 main fragment로 이동
@@ -47,5 +65,17 @@ class LoginFragment : Fragment() {
                 findNavController().popBackStack()
             }
         })
+    }
+
+    suspend fun RoomDBInsertUserData(mc: MainActivity, newUserData: UserData){
+        mc.db.userDao().insertUserData(newUserData)
+    }
+
+    private suspend fun MoveMainFragment(){
+        CoroutineScope(Dispatchers.Main).launch {
+            // room DB 체크해서 넘어갈지 말지
+            findNavController().navigate(R.id.action_frag_login_to_frag_main)
+        }
+
     }
 }
